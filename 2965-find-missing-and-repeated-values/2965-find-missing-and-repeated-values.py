8880fc64f2e108617a519e5acc0750fdfd0475f9
Amal S Krishna <amalskrishna06@gmail.com>
2026-06-18 10:55:12 +0530

    Time: 2 ms (93.97%), Space: 19.6 MB (39.74%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -1,22 +1,24 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        l=len(grid)
-        sum1=0
-        sumsq=0
-        n=l*l
+        n = len(grid)
+        seen = set()
+        repeated = -1
+
+        #repeated number
         for i in grid:
             for j in i:
-                sum1 +=j
-                sumsq +=j*j
-        exsum=(n*(n+1))//2
-        exsumsq= n*(n+1)*(2*n+1)//6
+                if j in seen:
+                    repeated = j
+                seen.add(j)
 
-        diff=sum1-exsum
-        diffsq=sumsq-exsumsq
-        
-        sumxy = diffsq//diff
-        
-        repeated= (diff+sumxy)//2
-        missing=repeated-diff
-        return [abs(repeated),abs(missing)]
+        #missing number
+        total_numbers = n * n
+        missing = -1
+
+        for i in range(1, total_numbers + 1):
+            if i not in seen:
+                missing = i
+                break
+
+        return [repeated, missing]
 
