305488a43796345cdb0f72ab3b86c27c4b038710
Amal S Krishna <amalskrishna06@gmail.com>
2026-06-27 09:12:15 +0530

    Time: 120 ms (39.86%), Space: 56.7 MB (88.91%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,17 +2,26 @@ class RandomizedSet:
 
     def __init__(self):
         self.arr=[]
+        self.indices={}
 
     def insert(self, val: int) -> bool:
-        if val in self.arr:
+        if val in self.indices:
             return False
         self.arr.append(val)
+        self.indices[val]=len(self.arr)-1
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.arr:
+        if val not in self.indices:
             return False
-        self.arr.remove(val)
+        ind=self.indices[val]
+        last=self.arr[-1]
+
+        self.arr[ind]=last
+        self.indices[last]=ind
+        
+        self.arr.pop()
+        del self.indices[val]
         return True
 
     def getRandom(self) -> int:
